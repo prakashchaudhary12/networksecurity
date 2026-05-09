4c025a8ffeee9e54579790086e3612ec17a19316
prakashchaudhary12 <chyprakash2002@gmail.com>
2026-05-09 13:21:16 +0530

    Setup file updated

---
+++ setup.py
@@ -0,0 +1,6 @@
+'''
+The setup.py file is an essential part of packaing and distribution 
+Python projects. It is used by setuptools
+(or distutils in older Python versions) to defile the
+configuration of your project, such as its metadata, dependencies, and more
+'''
